ae81719a1f938cc0eb716e7bcb8179ce445d3899
haris-afr <haris.afridi1312@gmail.com>
2025-12-13 21:27:03 +0500

    sort libraries

--- test.py
+++ test.py
@@ -1,13 +1,20 @@
-# This project requires the "pandas" and "matplotlib" libraries.
+# This project requires the "pandas" and "matplotlib" libraries, for .
 # In case either is not installed please follow the instructions on their respective websites
 # https://pandas.pydata.org/docs/getting_started/install.html   <--- pandas
 # https://matplotlib.org/stable/users/getting_started/          <--- matplotlib
 
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-import tkinter
-import time
+# the mouse library is also used, it can be installed using pip install mouse.
+
+#-------------------------------------------------------------------------
+# import pandas as pd #used for data organisation, may be redundant due to numpy
+# import time
+# import mouse #user input, may be redundant due to tkinter
+#--------------------------------------------------------------
+
+import matplotlib.pyplot as plt #drawing graphs
+import numpy as np #used for arrays
+import tkinter #GUI 
+
 
 a = np.array([1, 2, 5, 6, 3, 4])
 b = np.array ([1, 2, 3, 4, 5, 6])
@@ -16,10 +23,10 @@ plt.bar(a, b)
 #test using binary sort
 for i in range(sizeA):
     for j in range(sizeA-1):
-        if (a[j + 1] < a[j]):
-            temp = a[j+1]
-            a[j+1] = a[j]
-            a[j] = temp
+        # if (a[j + 1] < a[j]):
+        #     temp = a[j+1]
+        #     a[j+1] = a[j]
+        #     a[j] = temp
         plt.clf()
         plt.draw()
         plt.pause(0.5)
