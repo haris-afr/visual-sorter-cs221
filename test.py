ce5124712d91d15c4406aeef7c34a16b9c4663ca
haris-afr <haris.afridi1312@gmail.com>
2025-12-13 21:38:22 +0500

    holy balls its working

--- test.py
+++ test.py
@@ -19,17 +19,19 @@ import tkinter #GUI
 a = np.array([1, 2, 5, 6, 3, 4])
 b = np.array ([1, 2, 3, 4, 5, 6])
 sizeA = len(a)
-plt.bar(a, b)
+
+plt.ioff()
+plt.draw()
+
 #test using binary sort
 for i in range(sizeA):
     for j in range(sizeA-1):
-        # if (a[j + 1] < a[j]):
-        #     temp = a[j+1]
-        #     a[j+1] = a[j]
-        #     a[j] = temp
+        if (a[j + 1] < a[j]):
+            temp = a[j+1]
+            a[j+1] = a[j]
+            a[j] = temp
         plt.clf()
-        plt.draw()
+        plt.bar(a, b)
         plt.pause(0.5)
 
-plt.ioff()
 plt.show()
