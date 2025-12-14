fa2d2e3190a19a26932f5492f7bb91bd2f637331
haris-afr <haris.afridi1312@gmail.com>
2025-12-14 20:05:54 +0500

    create frames for structure

--- main.py
+++ main.py
@@ -9,11 +9,27 @@ mainWindow.geometry("600x400")
 ctk.FontManager.load_font("fonts/Courier_Prime/CourierPrime.ttf")
 if ("Courier Prime" in font.families()):
     heading_font = ctk.CTkFont(family="Courier Prime", size=38)
+    label_font = ctk.CTkFont(family="Courier Prime Bold", size=16)
 else: #Fall back font
     heading_font = ctk.CTkFont(family="Courier New Bold", size=42)
+    heading_font = ctk.CTkFont(family="Courier New Bold", size=20)
 
 
-w = ctk.CTkLabel(mainWindow, text="DSA Visualizer", font=heading_font)
-w.pack()
+headingLabel = ctk.CTkLabel(mainWindow, text="DSA Visualizer", font=heading_font)
+
+algorithmFrame = ctk.CTkFrame(mainWindow, fg_color="transparent")
+selectAlgorithmLabel = ctk.CTkLabel(algorithmFrame, text="Select Algorithm", font=label_font)
+
+arrayFrame = ctk.CTkFrame(mainWindow, fg_color="transparent")
+inputArrayLabel = ctk.CTkLabel(arrayFrame, text="Input Array", font=label_font)
+
+runButton = ctk.CTkButton(mainWindow, text='Run')
+
+headingLabel.pack(pady="10px")
+algorithmFrame.pack()
+selectAlgorithmLabel.pack()
+arrayFrame.pack()
+inputArrayLabel.pack()
+runButton.pack()
 
 mainWindow.mainloop()
