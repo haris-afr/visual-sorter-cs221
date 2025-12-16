9d2117b91b9d25f50417d64149fe121f14f1fd97
haris-afr <haris.afridi1312@gmail.com>
2025-12-16 18:46:09 +0500

    implment scenme change

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import font
 import customtkinter as ctk
+import re #regex
 
 #creating window
 mainWindow = ctk.CTk()
@@ -10,7 +11,8 @@ mainWindow.geometry("600x400")
 #creating list of algorithms
 algorithm_array = ["Bubble Sort", "Insertion Sort", "Selection Sort", "Count Sort",
                     "Merge Sort", "Quick Sort", "Custom"]
-selectedAlg = ctk.StringVar(value=algorithm_array[0])
+selectedAlg = ctk.StringVar()
+textA = ""
 
 #loading fonts
 ctk.FontManager.load_font("fonts/Courier_Prime/CourierPrime.ttf")
@@ -23,6 +25,42 @@ else: #Fall back font
     label_font = ctk.CTkFont(family="Courier New Bold", size=20)
     info_font = ctk.CTkFont(family="Courier New Bold", size=14)
 
+#declaring functions
+def changeToScene2():
+    headingLabel.pack_forget()
+    algorithmSelectLabel.pack_forget()
+    algorithmList.pack_forget()
+    algorithmFrame.pack_forget()
+    arrayInputLabel.pack_forget()
+    arrayTextBox.pack_forget()
+    arrayFrame.pack_forget()
+    runButton.pack_forget()
+
+def changeToScene1():
+    headingLabel.pack(pady="10px")
+    elementPadding = 10
+    algorithmSelectLabel.pack(side="left", padx=f"{elementPadding}px")
+    algorithmList.pack(side="left", padx=f"{elementPadding}px")
+    algorithmFrame.pack(pady="15px")
+    arrayInputLabel.pack(side="left")
+    arrayTextBox.pack()
+    arrayFrame.pack(pady='15px')
+    runButton.pack()
+
+
+
+def getText(textbox: ctk.CTkTextbox, errorLabel:ctk.CTkLabel):
+    global textA
+    textA = textbox.get('0.0', 'end')
+    textA = re.findall("^\[\d+(?:,\s*\d+)*\]$", textA)
+    if (textA.__len__() == 0):
+        errorLabel.pack()
+    else:
+        errorLabel.pack_forget()
+        changeToScene2()
+        return textA
+
+
 
 #creating elements
 headingLabel = ctk.CTkLabel(mainWindow, text="DSA Visualizer", font=heading_font)
@@ -34,21 +72,29 @@ algorithmList = ctk.CTkComboBox(algorithmFrame, values=algorithm_array, variable
 
 arrayFrame = ctk.CTkFrame(mainWindow, fg_color="transparent")
 arrayInputLabel = ctk.CTkLabel(arrayFrame, text="Input Array", font=label_font)
+arrayTextBox = ctk.CTkTextbox(arrayFrame, height = 100)
 
-runButton = ctk.CTkButton(mainWindow, text='Run')
+errorLabel = ctk.CTkLabel(mainWindow, text="Error! Incorrect Array!", font=label_font)
+
+runButton = ctk.CTkButton(mainWindow, text='Run', command=lambda: getText(arrayTextBox, errorLabel))
+
+#creating elements for scene 2
+
 
 #displaying all of the elements
-headingLabel.pack(pady="10px")
+# headingLabel.pack(pady="10px")
 
-elementPadding = 10
-algorithmSelectLabel.pack(side="left", padx=f"{elementPadding}px")
-algorithmList.pack(side="left", padx=f"{elementPadding}px")
-algorithmFrame.pack(pady="15px")
+# elementPadding = 10
+# algorithmSelectLabel.pack(side="left", padx=f"{elementPadding}px")
+# algorithmList.pack(side="left", padx=f"{elementPadding}px")
+# algorithmFrame.pack(pady="15px")
 
-arrayInputLabel.pack(side="left")
-arrayFrame.pack()
+# arrayInputLabel.pack(side="left")
+# arrayTextBox.pack()
+# arrayFrame.pack(pady='15px')
 
-runButton.pack()
+# runButton.pack()
+
 
 
 mainWindow.mainloop()
