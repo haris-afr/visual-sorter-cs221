972f55d352d6427cba0796f5c279f8797ce702ab
haris-afr <haris.afridi1312@gmail.com>
2025-12-16 19:32:41 +0500

    move algorithms to new file

--- image_gen.py
+++ image_gen.py
@@ -30,3 +30,53 @@ def bubble_sort(vals):
             return loop_called
     return loop_called
 
+#Sorting Algorithms 
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            #Add screenshot here
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+            #Add screenshot here
+    #Final screenshot here
+
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        while j>=-1 and key < arr[j]:
+            arr[j+1] = arr[j]
+            #Add screenshot here
+            j-=1
+        arr[j+1] = key
+        #Add screenshot here
+    #Final screenshot here
+   
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_index = i
+        for j in range(i+1,len(arr)):
+            #Add screenshot here
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i],arr[min_index] = arr[min_index],arr[i]
+        #Add screenshot here
+    #Final screenshot here
+   
+def start_Algo():
+    inputArr = [int(x) for x in arrayInputLabel.get().split(',')]
+    algorithm = selectedAlg.get()
+
+    if algorithm == "Bubble Sort":
+        bubble_sort(inputArr)
+
+    elif algorithm == "Insertion Sort":
+        insertion_sort(inputArr)
+
+    elif algorithm == "Selection Sort":
+        selection_sort(inputArr)
+
