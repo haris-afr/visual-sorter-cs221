5dc17e7f4692f53251c97d1cefea9d86f3a65d9b
aqibfaisal006 <aqibfaisal9@gmail.com>
2025-12-16 18:32:59 +0500

    added insertion sort

--- main.py
+++ main.py
@@ -59,7 +59,19 @@ def bubble_sort(arr):
     for i in range(n):
         for j in range(0, n-i-1):
             if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j];
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        while j>=-1 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j-=1
+        arr[j+1] = key
+    return arr
 
 
 mainWindow.mainloop()
