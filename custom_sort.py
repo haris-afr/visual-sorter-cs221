4002255a072b128515e5fa8ddd53ee25e2efa73c
haris-afr <haris.afridi1312@gmail.com>
2025-12-17 14:31:17 +0500

    final3

--- custom_sort.py
+++ custom_sort.py
@@ -1,18 +1,22 @@
 #Custom sorter, simply write the algorithm you want in between the comments
 #and take screenshots everytime something changes, then run it form the custom setting
 #in the main program
-
-
 import numpy as np
+from image_gen import take_screenshot
+
+
 
 def custom_sort(arr):
-    Pictures_taken = 0
+    pictures_taken = 0
     arr = np.array(arr)
     n = len(arr)
     indices = np.array([i+1 for i in range(n)])
+
+    #copy paste this line when taking a picture
+    pictures_taken = take_screenshot(pictures_taken, arr, indices)
 
     #write code here-----
 
     #--------------------
 
-    return Pictures_taken
+    return pictures_taken
